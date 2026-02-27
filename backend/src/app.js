2d2d56a8e2aedd4893d643668ea058321d01fa03
Ankit Mohanty <ankitmsb760@gmail.com>
2026-02-28 01:10:41 +0530

    setup: configure express app with global middleware and route mounting

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import globalErrorHandler from "../src/middlewares/error.middleware.js";
+import logger from "./utils/logger.js";
+import userRoutes from "./routes/user.route.js";
+
+const app = express();
+
+app.use(cors());
+//app.use(helmet());
+app.use(
+    morgan("dev", {
+        stream: {
+            write: (message) => logger.info(message.trim()),
+        },
+    })
+);
+app.use(express.json());
+
+app.use("/api/v1/user", userRoutes);
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "SmartQ API running",
+    });
+});
+
+app.use(globalErrorHandler);
+
+export default app;
