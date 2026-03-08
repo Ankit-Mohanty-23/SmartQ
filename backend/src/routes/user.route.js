d8a8305021b31d2ddd8533a9983b7730841a9fb4
Ankit Mohanty <ankitmsb760@gmail.com>
2026-03-08 23:48:40 +0530

    feat(users): add user API routes with proper input validation

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -6,33 +6,35 @@ import auth from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-/**
- * @route   POST /api/v1/user/register
- * @desc    Register hospital staff
- * @access  Public (Can restrict to ADMIN later)
- */
 router.post(
   "/register",
   validate(userValidator.registerUserSchema),
   userController.registerUser,
 );
-
-/**
- * @route   POST /api/v1/user/login
- * @desc    Login hospital staff
- * @access  Public
- */
 router.post(
   "/login",
   validate(userValidator.loginUserSchema),
   userController.loginUser,
 );
-
-/**
- * @route   GET /api/v1/user/me
- * @desc    Get current logged-in user
- * @access  Private
- */
 router.get("/me", auth, userController.getCurrentUser);
+router.post("/logout", auth, userController.userLogout);
+router.get(
+  "/",
+  auth,
+  validate(userValidator.getAllUsersSchema),
+  userController.getAllUsers,
+);
+router.put(
+  "/:id",
+  auth,
+  validate(userValidator.updateUserSchema),
+  userController.updateUser,
+);
+router.patch(
+  "/:id/deactivate",
+  auth,
+  validate(userValidator.deactivateUserSchema),
+  userController.deactivateUser,
+);
 
 export default router;
