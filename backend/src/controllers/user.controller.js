702d12c9bdd8e38f6a8a0b5651fe8a4eb33ae786
Ankit Mohanty <ankitmsb760@gmail.com>
2026-03-08 23:47:59 +0530

    feat(users): implement user controller handlers

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 /**
  * @desc    Register hospital staff
- * @route   POST /api/v1/auth/register
+ * @route   POST /api/v1/users/register
  * @access  Public (or ADMIN controlled later)
  */
 
@@ -18,7 +18,7 @@ export const registerUser = asyncHandler(async (req, res) => {
 
 /**
  * @desc    Login hospital staff
- * @route   POST /api/v1/auth/login
+ * @route   POST /api/v1/users/login
  * @access  Public
  */
 
@@ -33,7 +33,7 @@ export const loginUser = asyncHandler(async (req, res) => {
 
 /**
  * @desc    Get logged-in user
- * @route   GET /api/v1/auth/me
+ * @route   GET /api/v1/users/me
  * @access  Private
  */
 
@@ -45,3 +45,67 @@ export const getCurrentUser = async (req, res) => {
     data: user,
   });
 };
+
+
+/**
+ * @desc    User logout
+ * @route   POST /api/v1/users/logout
+ * @access  Public
+ */
+
+export const userLogout = asyncHandler(async(req, res) => {
+  const logout = await userLogoutService();
+
+  res.clearCookie("token");
+
+  res.status(200).json({
+    success: logout.success,
+    message: logout.message
+  });
+});
+
+/**
+ * @desc    Get All Users Service
+ * @route   POST /api/v1/users
+ * @access  Public
+ */
+
+export const getAllUsers = asyncHandler( async(req, res) => {
+  const users = await userService.getAllUsersService(req.query);
+
+  res.status(200).json({
+    success: true,
+    data: users
+  });
+});
+
+/**
+ * @desc    Update User Service
+ * @route   POST /api/v1/users/:id
+ * @access  Public
+ */
+
+export const updateUser = asyncHandler( async(req, res) => {
+  const user = await userService.updateUserService(req.params.id, req.body);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+/**
+ * @desc    Deactivate User Service
+ * @route   POST /api/v1/users/:id/deactivate
+ * @access  Public
+ */
+
+export const deactivateUser = asyncHandler( async(req, res) => {
+  const userId = req.params.id;
+  const user = await userService.deactivateUserService(userId);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+})
