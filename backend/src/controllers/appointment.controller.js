ca4536362026904b54a171974af7edb15a9e90f4
Ankit Mohanty <ankitmsb760@gmail.com>
2026-03-18 01:38:08 +0530

    Create controller handling appointment CRUD operations

---
+++ backend/src/controllers/appointment.controller.js
@@ -0,0 +1,133 @@
+import * as appointmentService from "../services/appointment.service.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+/**
+ * @desc    Create Appointment
+ * @route   GET /api/v1/appointments
+ * @access  Public
+ */
+
+export const createAppointment = asyncHandler(async (req, res) => {
+  const {
+    name,
+    phone,
+    patientAge,
+    patientGender,
+    problem,
+    visitType,
+    preferredDate,
+  } = req.body;
+
+  const appointment = await appointmentService.createAppointmentService({
+    name,
+    phone,
+    problem,
+    visitType,
+    patientAge,
+    patientGender,
+    preferredDate,
+  });
+
+  res.status(201).json({
+    success: true,
+    data: appointment,
+  });
+});
+
+/**
+ * @desc    List all Appointments
+ * @route   GET /api/appointments?
+ *              status=PENDING&
+ *              preferredDate=2026-03-15&
+ *              assignedDoctorId=uuid-here
+ * @access  Private
+ */
+
+export const listAppointment = asyncHandler(async (req, res) => {
+  const { status, preferredDate, assignedDoctorId } = req.query;
+
+  const appointments = await appointmentService.listAppointmentService({
+    status,
+    preferredDate,
+    assignedDoctorId,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: appointments,
+  });
+});
+
+/**
+ * @desc    Get Appointment Detail
+ * @route   GET /api/appointments/:appointmentId
+ * @access  Private
+ */
+
+export const appointmentInfo = asyncHandler(async (req, res) => {
+  const { appointmentId } = req.params;
+
+  const appointment =
+    await appointmentService.appointmentInfoService(appointmentId);
+
+  res.status(200).json({
+    success: true,
+    data: appointment,
+  });
+});
+
+/**
+ * @desc    Convert appointment to token
+ * @route   GET /api/v1/appointments/convert
+ * @access  Private
+ */
+
+export const handleConvertToToken = asyncHandler(async (req, res) => {
+  const { doctorProfileId, appointmentId } = req.body;
+
+  const result = await appointmentService.convertAppointmentService(
+    req.user.id,
+    appointmentId,
+    doctorProfileId,
+  );
+
+  res.status(200).json({
+    success: true,
+    data: result,
+  });
+});
+
+/**
+ * @desc    Reject Appointment request
+ * @route   GET /api/v1/appointments/:appointmentId/reject
+ * @access  Private
+ */
+
+export const handleRejectAppointment = asyncHandler(async (req, res) => {
+  const { appointmentId } = req.params;
+
+  const appointment =
+    await appointmentService.rejectAppointmentService(appointmentId);
+
+  res.status(200).json({
+    success: true,
+    data: appointment,
+  });
+});
+
+/**
+ * @desc    Reject Appointment request
+ * @route   GET /api/v1/appointments/:appointmentId/cancel
+ * @access  Private
+ */
+
+export const handleCancelAppointment = asyncHandler(async (req, res) => {
+  const { appointmentId } = req.params;
+
+  const appointment = await appointmentService.cancelAppointment(appointmentId);
+
+  res.status(200).json({
+    success: true,
+    data: appointment,
+  });
+});
