cebc2c3f6386539cf06f52de74110a6ed6bd3ba0
Ankit Mohanty <ankitmsb760@gmail.com>
2026-04-01 01:16:31 +0530

    Expose new queue management features via updated route definitions

--- backend/src/routes/queue.route.js
+++ backend/src/routes/queue.route.js
@@ -1,37 +1,43 @@
 import { Router } from "express";
 import * as queueController from "../controllers/queue.controller.js";
-import auth from "../middlewares/auth.middleware.js";
+import { auth, restrictTo } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import {
-  createQueueSchema,
-  updateQueueStatusSchema,
-  trackQueueSchema,
-} from "../validations/queue.validate.js";
+import * as queueValidatation from "../validations/queue.validate.js";
 
 const router = Router();
 
-/**
- * Staff Protected Routes
- */
-router.post(
-  "/",
-  auth,
-  validate(createQueueSchema),
-  queueController.createQueue,
+router.get(
+  "/:tokenId/patient",
+  validate(queueValidatation.trackQueueSchema),
+  queueController.getPatientView,
+);
+router.patch(
+  "/:id/cancel",
+  queueController.cancelQueue,
 );
 
-router.patch("/:id/ongoing", auth, queueController.markQueueInProgress);
+router.use(auth);
 
-router.patch("/:id/complete", auth, queueController.markQueueCompleted);
-
-router.get("/:doctorId/track", auth, queueController.getDoctorQueue);
-
-router.patch("/:id/cancel", auth, queueController.cancelQueue);
-
-/**
- * Public Route
- */
-
-router.get("/:tokenId/patient", auth, queueController.getPatientView);
+router.get(
+  "/:doctorId/track",
+  restrictTo("ADMIN", "RECEPTIONIST", "DOCTOR"),
+  queueController.getDoctorQueue,
+);
+router.post(
+  "/",
+  restrictTo("ADMIN", "RECEPTIONIST"),
+  validate(queueValidatation.createQueueSchema),
+  queueController.createQueue,
+);
+router.patch(
+  "/:id/ongoing",
+  restrictTo("ADMIN", "DOCTOR"),
+  queueController.markQueueInProgress,
+);
+router.patch(
+  "/:id/complete",
+  restrictTo("ADMIN", "DOCTOR"),
+  queueController.markQueueCompleted,
+);
 
 export default router;
