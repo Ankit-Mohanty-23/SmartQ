468a4c961b8971eb7d76b144d1bb8b3f993fd2db
Ankit Mohanty <ankitmsb760@gmail.com>
2026-04-01 01:17:23 +0530

    Update user authentication and profile management endpoints

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,37 +2,40 @@ import { Router } from "express";
 import * as userController from "../controllers/user.controller.js";
 import * as userValidator from "../validations/user.validate.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import auth from "../middlewares/auth.middleware.js";
+import { auth, restrictTo } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+
+router.post(
+  "/login",
+  validate(userValidator.loginUserSchema),
+  userController.loginUser,
+);
+
+router.use(auth);
+
+router.get("/me", userController.getCurrentUser);
+router.post("/logout", userController.userLogout);
+
+router.use(restrictTo("ADMIN"));
 
 router.post(
   "/register",
   validate(userValidator.registerUserSchema),
   userController.registerUser,
 );
-router.post(
-  "/login",
-  validate(userValidator.loginUserSchema),
-  userController.loginUser,
-);
-router.get("/me", auth, userController.getCurrentUser);
-router.get("/logout", auth, userController.userLogout);
 router.get(
   "/",
-  auth,
   validate(userValidator.getAllUsersSchema),
   userController.getAllUsers,
 );
 router.put(
   "/:id",
-  auth,
   validate(userValidator.updateUserSchema),
   userController.updateUser,
 );
 router.patch(
   "/:id/deactivate",
-  auth,
   validate(userValidator.deactivateUserSchema),
   userController.deactivateUser,
 );
