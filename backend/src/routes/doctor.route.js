40bf583db7a712e811d66b6d67768e245a1430a5
Ankit Mohanty <ankitmsb760@gmail.com>
2026-04-01 01:16:23 +0530

    Add new endpoints and update security for doctor-related actions

--- backend/src/routes/doctor.route.js
+++ backend/src/routes/doctor.route.js
@@ -2,16 +2,20 @@ import express from "express";
 import * as doctorController from "../controllers/doctor.controller.js";
 import { updateDoctorSchema } from "../validations/doctor.validate.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import auth from "../middlewares/auth.middleware.js";
+import { auth, restrictTo } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+router.use(auth);
 
-router.get("/", auth, doctorController.getAllDoctors);
-
-router.get("/:doctorProfileId", auth, doctorController.getDoctorById);
-
+router.get("/:doctorProfileId", doctorController.getDoctorById);
+router.get(
+  "/",
+  restrictTo("ADMIN", "RECEPTIONIST"),
+  doctorController.getAllDoctors,
+);
 router.put(
   "/:doctorProfileId",
+  restrictTo("ADMIN"),
   validate(updateDoctorSchema),
   doctorController.updateDoctor,
 );
