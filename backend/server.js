588aa92e040d894f57f70be552fbf205a09903f0
Ankit Mohanty <ankitmsb760@gmail.com>
2026-04-01 01:13:48 +0530

    Update server.js with enhanced middleware and service initialization

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import app from "./src/app.js";
 import { env } from "./src/config/env.js";
 import logger from "./src/utils/logger.js";
 import connectDB from "./src/config/db.js";
+import { registerDriftDetectorJob } from "./src/jobs/driftDetector.job.js";
+import { healthCheck } from "./src/services/ml.service.js";
 
 const PORT = env.PORT || 5000;
 
@@ -11,6 +13,9 @@ async function startServer(){
 
         app.listen(PORT, () => {
             logger.info(`Server running in port: ${PORT}`);
+            registerDriftDetectorJob();
+            healthCheck();
+            logger.info("Drift Detector Job registered successfully");
         });
         
     }catch(error){
