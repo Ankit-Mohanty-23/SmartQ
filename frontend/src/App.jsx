d20a50f2c55c134e28e2cf5c64eafa1703df3929
Ankit Mohanty <ankitmsb760@gmail.com>
2026-05-01 02:54:53 +0530

    Update main App component with new layouts and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,8 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import { useEffect } from "react";
-import { getCurrentUser } from "./services/authService";
 
 function App() {
-
-  useEffect(() => {
-    const loadUser = async () => {
-      try {
-        const user = await getCurrentUser(); 
-
-        localStorage.setItem("user", JSON.stringify(user));
-      } catch {
-        localStorage.removeItem("user");
-      }
-    };
-
-    loadUser();
-  }, []);
-
   return (
     <BrowserRouter>
       <AppRoutes />
