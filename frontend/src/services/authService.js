e630d893b57042d2fae7236074502d144381d833
Ankit Mohanty <ankitmsb760@gmail.com>
2026-05-01 02:54:55 +0530

    Update AuthService with new token handling and login logic

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,24 +2,24 @@ import API from "./api";
 
 // LOGIN
 export const loginUser = async (data) => {
-  const res = await API.post("/api/v1/users/login", data);
+  const res = await API.post("/users/login", data);
   return res.data.data;
 };
 
 // REGISTER
 export const registerUser = async (data) => {
-  const res = await API.post("/api/v1/users/register", data);
+  const res = await API.post("/users/register", data);
   return res.data.data;
 };
 
 // GET CURRENT USER
 export const getCurrentUser = async () => {
-  const res = await API.get("/api/v1/users/me");
+  const res = await API.get("/users/me");
   return res.data.data;
 };
 
 // LOGOUT
 export const logoutUser = async () => {
-  const res = await API.post("/api/v1/users/logout");
+  const res = await API.post("/users/logout");
   return res.data;
 };
